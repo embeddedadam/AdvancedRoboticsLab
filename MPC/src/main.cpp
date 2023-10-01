2df2ce82251e6ebc492dfbedb7f2a78554c7d3e6
Adam Galecki <embeddedadam@gmail.com>
2023-10-01 16:38:34 +0200

    Fix build system changes.
     - Fix gtest linking issue under every project.
     - Empty wrong MPC implementation, left only header definition.
     - Include MPC in build once again.

--- MPC/src/main.cpp
+++ MPC/src/main.cpp
@@ -2,14 +2,7 @@
 #include <iostream>
 
 int main() {
-    MPCController mpc(10, 0.1, 10.0);
-    Eigen::Vector2d x = Eigen::Vector2d::Zero();
 
-    for (int i = 0; i < 100; i++) {
-        double u = mpc.control(x);
-        x = Eigen::MatrixXd(2, 2) << 1, 0.1, 0, 1 * x + Eigen::MatrixXd(2, 1) << 0.5 * 0.1 * 0.1, 0.1 * u;
-        std::cout << "Step " << i << ": Position = " << x(0) << ", Velocity = " << x(1) << ", Control = " << u << std::endl;
-    }
 
     return 0;
 }
